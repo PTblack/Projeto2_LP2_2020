6ae3a22da073b7c9f80999954659a9202adc13d3
QuadQuasimodo <pedromafernandes@outlook.pt>
2020-12-28 16:59:12 +0000

    Add CheckPosition method

--- Project2_LP2_2020/BoardManager.cs
+++ Project2_LP2_2020/BoardManager.cs
@@ -2,6 +2,7 @@ namespace Project2_LP2_2020
 {
     public class BoardManager
     {
+        Board board = new Board();
         BoardSearcher boardSearcher = new BoardSearcher();
         private bool CheckWin (Piece placedPiece)
         {
@@ -18,6 +19,26 @@ namespace Project2_LP2_2020
                 return true;
             else 
                 return false;
+        }
+
+        /// <summary>
+        /// Checks if the number given by the player is a valid board column
+        /// (if it is in the board and if it isn't full)
+        /// </summary>
+        /// <param name="column"></param>
+        /// <returns></returns>
+        private bool CheckPosition(int column)
+        {
+            // Checks if the chosen column exists in the board
+            if (1 <= column <= board.lenght)
+                {
+                    // Checks if the column has space(s) available
+                    if (board.boardArray[column, board.height] == "e")
+                        return true;
+                    else 
+                        return false;
+                }
+            else return false;
         }
     }
 }
