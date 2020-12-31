f4951b41870d41a141bb7063f8ae525d84a499f6
diogo-h <diogo.m.henriques98@gmail.com>
2020-12-31 19:27:15 +0000

    Add BaseGameObject class
    Substitute the old version of component class

---
+++ Project2_LP2_2020/GameEngine/BaseGameObject.cs
@@ -0,0 +1,20 @@
+namespace Project2_LP2_2020.GameEngine
+{
+    public class BaseGameObject : IGameObject
+    {
+        public virtual void Start()
+        {
+
+        }
+        
+        public virtual void Update()
+        {
+
+        }
+
+        public virtual void Finish()
+        {
+
+        }
+    }
+}
